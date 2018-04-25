986a8deec3d66aabb08fa5ad4608ffc874b3a239
crj1111 <1587688471@qq.com>
2018-04-25 17:02:14 +0800

    流程定义页面添加角色定义 和变量定义，待完善

---
+++ centit-workflow-view/src/main/webapp/modules/sys/flowdefine/ctrl/flowRole/flowdefine.role.js
@@ -0,0 +1,60 @@
+define(function(require) {
+    var Config = require('config');
+    var Core = require('core/core');
+
+    var FlowDefineAdd = require('../../ctrl/flowdefine.add');
+
+    var FlowDefineStageAdd = require('../../ctrl/flowdefine.stage.add');
+    var FlowDefineStageRemove = require('../../ctrl/flowdefine.stage.remove');
+
+    // 编辑流程定义
+    var FlowDefineRole = FlowDefineAdd.extend(function() {
+        var _self = this;
+
+        this.injecte([
+            new FlowDefineStageAdd('flowdefine_stage_add'),
+            new FlowDefineStageRemove('flowdefine_stage_remove')
+        ]);
+
+        // @override
+        this.load = function(panel, data) {
+            var form = panel.find('form');
+            var table = this.table = panel.find('table');
+
+            Core.ajax(Config.ContextPath+'service/flow/define/draft/'+data.flowCode, {
+                method: 'get'
+            }).then(function(data) {
+                data = _self.extendData(data);
+
+                form.form('load', data);
+
+                table.cdatagrid({
+                    controller: _self
+                })
+                    .datagrid('loadData', data.wfFlowStages);
+            });
+        };
+
+        // @override
+        this.submit = function(panel, data, closeCallback) {
+            var form = panel.find('form');
+            var table = this.table;
+
+            if (form.form('validate') && table.cdatagrid('endEdit')) {
+                var items = table.datagrid('getData').rows;
+
+                data.wfFlowStages = items;
+                data._method = 'PUT';
+
+                Core.ajax(Config.ContextPath+'service/flow/define/stage/'+data.flowCode, {
+                    data: data,
+                    method: 'post'
+                }).then(closeCallback);
+
+                return false;
+            }
+        };
+    });
+
+    return FlowDefineRole;
+});
