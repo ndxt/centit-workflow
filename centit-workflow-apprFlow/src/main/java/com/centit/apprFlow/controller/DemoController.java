ece69b0609a41663d4672a555c94d18393daaea8
crj1111 <1587688471@qq.com>
2018-05-07 10:46:01 +0800

    apprFlow 第一次提交

---
+++ centit-workflow-apprFlow/src/main/java/com/centit/apprFlow/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.centit.apprFlow.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by chen_rj on 2018-4-27.
+ */
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+}
