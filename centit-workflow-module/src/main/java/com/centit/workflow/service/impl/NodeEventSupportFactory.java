63956f0cde699c00bad14b8ebdf253481bd6c8f6
杨淮生 <codefan@centit.com>
2018-06-11 08:58:39 +0800

    重构事件工厂

--- centit-workflow-module/src/main/java/com/centit/workflow/service/impl/NodeEventSupportFactory.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/impl/NodeEventSupportFactory.java
@@ -1,17 +1,11 @@
 package com.centit.workflow.service.impl;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.centit.support.file.FileIOOpt;
+import com.centit.framework.ip.po.OsInfo;
+import com.centit.framework.ip.service.IntegrationEnvironment;
 import com.centit.workflow.commons.NodeEventSupport;
 import com.centit.workflow.po.NodeInfo;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
-
-import javax.servlet.ServletContext;
-import java.io.File;
-import java.io.IOException;
 
 /**
  * 根据 OSID
@@ -20,52 +14,29 @@ import java.io.IOException;
  * @version
  */
 public class NodeEventSupportFactory {
-    private static JSONArray osInfos = null;
+
+    private static IntegrationEnvironment integrationEnvironment = null;
+
     public static NodeEventSupport getNodeEventSupportBean(NodeInfo nodeInfo){
         if(nodeInfo == null){
             return  null;
         }
+
         WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
-        ServletContext servletContext = webApplicationContext.getServletContext();
-        Boolean isLocal = true;
-        String osId  =nodeInfo.getOsId();
-        String url = "";
-        NodeEventSupport nodeEventSupport = null;
-        if(osInfos == null){
-            JSONObject json = null;
-            try {
-                String jsonFile = Thread.currentThread().getContextClassLoader().getResource("").getPath()+"ip_environmen.json";
-                File ipConfigFile = new File(jsonFile);
-                if(ipConfigFile.exists() && !ipConfigFile.isDirectory()){
-                    String jsonStr = FileIOOpt.readStringFromFile(jsonFile,"utf-8");
-                    json = JSON.parseObject(jsonStr);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if(json != null ){
-                osInfos = JSONArray.parseArray(json.get("osInfos").toString());
-            }
+        if(integrationEnvironment == null) {
+            integrationEnvironment = (IntegrationEnvironment)
+                    webApplicationContext.getBean("integrationEnvironment");
         }
-        if(osInfos != null && osInfos.size() > 0){
-            for(int i = 0;i < osInfos.size();i++){
-                JSONObject osInfo = (JSONObject)osInfos.get(i);
-                if(osId != null && osId.equals(osInfo.get("osId"))){
-                    url = (String) osInfo.get("osUrl");
-                    isLocal = false;
-                    break;
-                }
-            }
-        }
-        if(!isLocal){
+
+        OsInfo osInfo = integrationEnvironment.getOsInfo(nodeInfo.getOsId());
+        if(osInfo != null){
             RemoteBeanNodeEventSupport remoteNodeEventExecutor = new RemoteBeanNodeEventSupport();
-            remoteNodeEventExecutor.setUrl(url);
-            nodeEventSupport = remoteNodeEventExecutor;
+            remoteNodeEventExecutor.setUrl(osInfo.getOsUrl());
+            return remoteNodeEventExecutor;
         }else{
             LocalBeanNodeEventSupport localNodeEventExecutor = new LocalBeanNodeEventSupport();
-            localNodeEventExecutor.setApplication(servletContext);
-            nodeEventSupport = localNodeEventExecutor;
+            localNodeEventExecutor.setApplication(webApplicationContext.getServletContext());
+            return localNodeEventExecutor;
         }
-        return nodeEventSupport;
     }
 }
