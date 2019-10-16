3f30b3c0280be81c584f5a1706f8f8440941dfc4
zhf <zhf@centit.com>
2019-10-16 14:09:13 +0800

    增加权限表达式接口

--- centit-workflow-module/src/main/java/com/centit/workflow/controller/RoleFormulaController.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/controller/RoleFormulaController.java
@@ -11,6 +11,7 @@ import com.centit.workflow.po.RoleFormula;
 import com.centit.workflow.service.RoleFormulaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -74,5 +75,24 @@ public class RoleFormulaController extends BaseController {
         JSONArray listObjects = roleFormulaService.viewRoleFormulaUsers(formulaCode);
         return listObjects;
     }
-
+    @ApiOperation(value = "预览权限表达式对应用户", notes =
+        "表达式为itemExp ([或| itemExp][与& itemExp][非! itemExp])的形式，itemExp为下列形式\n" +
+            "D()P()DT()DL()GW()XZ()R()UT()UL()U()RO()\n" +
+            "* D 根据机构代码过滤 D(机构表达式)\n" +
+            "* P 根据机构代码过滤主要机构\n" +
+            "* DT 根据机构类型过滤 DT(\"角色代码常量\" [,\"角色代码常量\"])\n" +
+            "* DL 根据机构标签过滤 DL(\"角色代码常量\" [,\"角色代码常量\"])\n" +
+            "* GW 根据岗位过滤 GW(\"角色代码常量\" [,\"角色代码常量\"])\n" +
+            "* XZ 根据行政职务过滤 XZ(\"角色代码常量\" [,\"角色代码常量\"])\n" +
+            "* R 根据行政职务等级过滤 R(U) / R(U-) / R(U-1) / R(U--) /R(U-1--)\n" +
+            "* U 根据用户代码过滤 U(用户变量|\"用户代码常量\" [,用户变量|\"用户代码常量])\n" +
+            "* UT 根据用户类型过滤 UT(\"用户类型常量\" [,\"用户类型常量\"])\n" +
+            "* UL 根据用户标签过滤 UL(\"用户标记常量\" [,\"用户标记常量\"])\n" +
+            "* RO 根据用户角色过滤 RO(\"系统角色代码常量\" [,\"系统角色代码常量\"])" )
+    @WrapUpResponseBody
+    @RequestMapping(value="/viewFormulaUsers/{formula}",method = RequestMethod.GET)
+    public JSONArray viewFormulaUsers(@PathVariable String formula){
+        JSONArray listObjects = roleFormulaService.viewFormulaUsers(StringEscapeUtils.unescapeHtml4(formula));
+        return listObjects;
+    }
 }
