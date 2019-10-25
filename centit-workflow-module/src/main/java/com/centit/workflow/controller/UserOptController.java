9f3822a47c505467f8e5566a054e17aa86644127
codefan <codefan@sina.com>
2019-10-25 17:29:23 +0800

    添加 获取用户 行政和岗位角色的接口

--- centit-workflow-module/src/main/java/com/centit/workflow/controller/UserOptController.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/controller/UserOptController.java
@@ -2,15 +2,20 @@ package com.centit.workflow.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.centit.framework.common.WebOptUtils;
+import com.centit.framework.components.SysUserFilterEngine;
 import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.core.controller.WrapUpResponseBody;
 import com.centit.framework.core.dao.PageQueryResult;
+import com.centit.framework.model.adapter.UserUnitFilterCalcContext;
+import com.centit.framework.model.basedata.IUserUnit;
 import com.centit.framework.security.model.CentitUserDetails;
 import com.centit.support.database.utils.PageDesc;
 import com.centit.workflow.po.RoleRelegate;
 import com.centit.workflow.service.FlowManager;
-import com.centit.workflow.service.RoleFormulaService;
+import com.centit.workflow.service.UserUnitFilterCalcContextFactory;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +23,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 用户的权限委托 和任务转移
@@ -30,10 +37,37 @@ import java.util.List;
 public class UserOptController extends BaseController {
     @Autowired
     private FlowManager flowManager;
-    @Autowired
-    private RoleFormulaService flowRoleService;
-    //private ResponseMapData resData;
 
+    @Autowired
+    private UserUnitFilterCalcContextFactory userUnitFilterFactory;
+
+    @ApiOperation(value = "获取用户角色", notes = "获取用户角色")
+    @ApiImplicitParams({@ApiImplicitParam(
+        name = "userCode", value="用户代码",
+        required=true, paramType = "path", dataType= "String"
+    ),@ApiImplicitParam(
+        name = "roleType", value="角色类别只能为'GW'或者'XZ'",
+        required=true, paramType = "path", dataType= "String"
+    )})
+    @WrapUpResponseBody
+    @GetMapping(value = "/role/{userCode}/{roleType}")
+    public Map<String, String> listUserRoles(@PathVariable String userCode,
+                                             @PathVariable String roleType){
+        UserUnitFilterCalcContext context = userUnitFilterFactory.createCalcContext();
+        List<? extends IUserUnit> userUnits = context.listUserUnits(userCode);
+        boolean isGwRole = SysUserFilterEngine.ROLE_TYPE_GW.equals(roleType);
+        Map<String, String> allRoles = isGwRole? context.listAllStation()
+            : context.listAllRank();
+        Map<String, String> userRoles = new HashMap<>();
+        for(IUserUnit uu : userUnits){
+            if(isGwRole){
+                userRoles.put(uu.getUserStation(), allRoles.get(uu.getUserStation()));
+            } else {
+                userRoles.put(uu.getUserRank(), allRoles.get(uu.getUserRank()));
+            }
+        }
+        return userRoles;
+    }
 
     /**
      * 获取指定用户委托列表
