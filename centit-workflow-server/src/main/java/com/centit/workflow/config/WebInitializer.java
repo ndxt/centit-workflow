9b03d647fc7b2d840ca081ac73ffb89d76512ba5
zhf <zhf@centit.com>
2019-10-25 17:37:07 +0800

    更新

--- centit-workflow-server/src/main/java/com/centit/workflow/config/WebInitializer.java
+++ centit-workflow-server/src/main/java/com/centit/workflow/config/WebInitializer.java
@@ -10,7 +10,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import javax.servlet.ServletRegistration.Dynamic;
-
 /**
  * Created by zou_wy on 2017/3/29.
  */
