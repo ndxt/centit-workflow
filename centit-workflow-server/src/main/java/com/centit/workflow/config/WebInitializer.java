780d6cd762862a4e1b95553f0786e8d7b7634575
zhf <zhf@centit.com>
2019-11-18 17:46:09 +0800

    更新

--- centit-workflow-server/src/main/java/com/centit/workflow/config/WebInitializer.java
+++ centit-workflow-server/src/main/java/com/centit/workflow/config/WebInitializer.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletException;
 /**
  * Created by zou_wy on 2017/3/29.
  */
-
 public class WebInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
