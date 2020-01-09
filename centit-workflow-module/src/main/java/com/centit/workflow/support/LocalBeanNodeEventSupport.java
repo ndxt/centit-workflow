4595a2d2f6299d288e3938ff0641c4cfb65ebf06
codefan <codefan@sina.com>
2020-01-09 09:00:24 +0800

    修改bug nullpointexception

--- centit-workflow-module/src/main/java/com/centit/workflow/support/LocalBeanNodeEventSupport.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/support/LocalBeanNodeEventSupport.java
@@ -5,6 +5,7 @@ import com.centit.workflow.commons.WorkflowException;
 import com.centit.workflow.po.FlowInstance;
 import com.centit.workflow.po.NodeInfo;
 import com.centit.workflow.po.NodeInstance;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -15,8 +16,8 @@ import javax.servlet.ServletContext;
 
 /**
  * @author codefan
- * @create 2013-7-10
- * @version
+ * create 2013-7-10
+ * @version v4.5
  */
 public class LocalBeanNodeEventSupport implements NodeEventSupport {
 
@@ -27,13 +28,16 @@ public class LocalBeanNodeEventSupport implements NodeEventSupport {
         this.application = application;
     }
 
-    private void checkRunCondition(NodeInfo nodeInfo, NodeInstance nodeInst){
-        if( nodeInfo.getOptBean()==null || "".equals(nodeInfo.getOptBean()))
-            throw new WorkflowException(WorkflowException.AutoRunNodeBeanNotFound,
-                "自动运行节点 " + nodeInst.getNodeInstId() +"出错，流程设置时没有设置节点的自动运行bean属性。");
+    private boolean checkRunCondition(NodeInfo nodeInfo, NodeInstance nodeInst){
+        if(StringUtils.isBlank(nodeInfo.getOptBean())){
+            return false;
+        }
+       /* throw new WorkflowException(WorkflowException.AutoRunNodeBeanNotFound,
+                "自动运行节点 " + nodeInst.getNodeInstId() +"出错，流程设置时没有设置节点的自动运行bean属性。");*/
         if(application==null)
             throw new WorkflowException(WorkflowException.AutoRunNodeWithoutApplcationContent,
                 "自动运行节点 " + nodeInst.getNodeInstId() +"出错，传递的参数application为空");
+        return true;
     }
 
     private  void logError(BeansException e, NodeInfo nodeInfo, NodeInstance nodeInst){
@@ -48,11 +52,12 @@ public class LocalBeanNodeEventSupport implements NodeEventSupport {
     public void runAfterCreate(FlowInstance flowInst, NodeInstance nodeInst,
                                NodeInfo nodeInfo, String optUserCode)
             throws WorkflowException {
-        checkRunCondition(nodeInfo, nodeInst);
-
+        if(!checkRunCondition(nodeInfo, nodeInst)){
+            return;
+        }
         try{
             WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(application);//获取spring的context
-            NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean() );
+            NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean());
             autoRun.runAfterCreate(flowInst, nodeInst, nodeInfo,optUserCode);
         }catch(BeansException e){
             logError(e, nodeInfo, nodeInst);
@@ -62,11 +67,12 @@ public class LocalBeanNodeEventSupport implements NodeEventSupport {
 
     public void runBeforeSubmit(FlowInstance flowInst, NodeInstance nodeInst,
                                        NodeInfo nodeInfo, String optUserCode) throws WorkflowException {
-
-        checkRunCondition(nodeInfo, nodeInst);
+        if(!checkRunCondition(nodeInfo, nodeInst)){
+            return;
+        }
         try{
             WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(application);//获取spring的context
-            NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean() );
+            NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean());
             autoRun.runBeforeSubmit(flowInst, nodeInst, nodeInfo,optUserCode);
         }catch(BeansException e){
             logError(e, nodeInfo, nodeInst);
@@ -79,7 +85,10 @@ public class LocalBeanNodeEventSupport implements NodeEventSupport {
     public boolean runAutoOperator(FlowInstance flowInst, NodeInstance nodeInst,
                                           NodeInfo nodeInfo, String optUserCode )
             throws WorkflowException {
-        checkRunCondition(nodeInfo, nodeInst);
+        if(!checkRunCondition(nodeInfo, nodeInst)){
+            throw new WorkflowException(WorkflowException.AutoRunNodeBeanNotFound,
+                "自动运行节点 " + nodeInst.getNodeInstId() +"出错，流程设置时没有设置节点的自动运行bean属性。");
+        }
         try{
             WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(application);//获取spring的context
             NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean() );
@@ -93,8 +102,10 @@ public class LocalBeanNodeEventSupport implements NodeEventSupport {
     @Override
     public boolean canStepToNext(FlowInstance flowInst, NodeInstance nodeInst,
                                  NodeInfo nodeInfo, String optUserCode) throws WorkflowException {
-
-        checkRunCondition(nodeInfo, nodeInst);
+        if(!checkRunCondition(nodeInfo, nodeInst)){
+            throw new WorkflowException(WorkflowException.AutoRunNodeBeanNotFound,
+                "条件判断节点 " + nodeInst.getNodeInstId() +"出错，流程设置时没有设置条件判断的bean属性。");
+        }
         try{
             WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(application);//获取spring的context
             NodeEventSupport autoRun = (NodeEventSupport) wac.getBean(nodeInfo.getOptBean() );
