9d0b37716e44efb9cc07d4d9edc48af6e249cda6
liu_cc <liu_cc@centit.com>
2020-04-20 16:08:57 +0800

    删除节点所有任务时，将节点实例taskAssigned改为T

--- centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowManagerImpl.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowManagerImpl.java
@@ -1021,6 +1021,11 @@ public class FlowManagerImpl implements FlowManager, Serializable {
         map.put("nodeInstId", nodeInstId);
         actionTaskDao.deleteObjectsByProperties(map);
 
+        // 删除节点所有任务时，将节点实例taskAssigned改为T
+        NodeInstance nodeInst = nodeInstanceDao.getObjectById(nodeInstId);
+        nodeInst.setTaskAssigned("T");
+        nodeInstanceDao.updateObject(nodeInst);
+
         OperationLog managerAct = FlowOptUtils.createActionLog(
             mangerUserCode, flowInstId, "删除节点所有任务:"
                 + nodeInstId);
