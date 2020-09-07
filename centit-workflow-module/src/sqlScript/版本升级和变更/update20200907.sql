9b168a10f917ddf32f4771f5668c082619cbc55c
codefan <codefan@sina.com>
2020-09-07 08:36:06 +0800

    升级到4.7-SNAPSHOT ，修改两个定义的表关联关系

---
+++ centit-workflow-module/src/sqlScript/版本升级和变更/update20200907.sql
@@ -0,0 +1,7 @@
+-- 将版本号升级到4.7 修改
+-- WF_FLOW_TEAM_ROLE 修改这表为 WF_OPT_TEAM_ROLE
+-- 可以新建这个表， 数据库中的表可以不动，代码中废弃这个表
+
+-- WF_FLOW_VARIABLE_DEFINE 修改这表为 WF_OPT_VARIABLE_DEFINE
+-- 同样可以新建这个表， 数据库中的表可以不动，代码中废弃这个表
+
