aaca7b6b5e1a4beddf3cc1cf3373e958d9453ffb
zhangbin <zhangbin@centit.com>
2020-09-11 10:58:43 +0800

    业务角色定义，业务变量 的维护接口

--- centit-workflow-module/src/main/java/com/centit/workflow/controller/FlowOptController.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/controller/FlowOptController.java
@@ -7,18 +7,18 @@ import com.centit.framework.core.controller.WrapUpResponseBody;
 import com.centit.framework.core.dao.PageQueryResult;
 import com.centit.framework.ip.po.OsInfo;
 import com.centit.framework.ip.service.IntegrationEnvironment;
+import com.centit.support.algorithm.CollectionsOpt;
 import com.centit.support.database.utils.PageDesc;
 import com.centit.workflow.po.FlowOptInfo;
 import com.centit.workflow.po.FlowOptPage;
+import com.centit.workflow.po.OptTeamRole;
+import com.centit.workflow.po.OptVariableDefine;
 import com.centit.workflow.service.FlowOptService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -168,5 +168,80 @@ public class FlowOptController extends BaseController {
         for(FlowOptPage optPage : optPages){
             wfOptService.saveOptPage(optPage);
         }
+    }
+
+
+    @GetMapping("/roles/{optId}")
+    @WrapUpResponseBody
+    @ApiOperation("根据optId获取角色定义列表")
+    public PageQueryResult<OptTeamRole> listOptTeamRolesByOptId(@PathVariable String optId, PageDesc pageDesc) {
+        Map<String, Object> filter = CollectionsOpt.createHashMap("optId", optId);
+        List<OptTeamRole> optTeamRoles = wfOptService.listOptTeamRolesByFilter(filter, pageDesc);
+        return PageQueryResult.createResult(optTeamRoles, pageDesc);
+    }
+
+    @GetMapping("/role/{roleId}")
+    @WrapUpResponseBody
+    @ApiOperation("根据主键id获取角色定义")
+    public OptTeamRole getOptTeamRoleById(@PathVariable String roleId) {
+        return wfOptService.getOptTeamRoleById(roleId);
+    }
+
+    @PostMapping("/role")
+    @WrapUpResponseBody
+    @ApiOperation("保存角色定义")
+    public void saveOptTeamRole(@RequestBody OptTeamRole optTeamRole) {
+        wfOptService.saveOptTeamRole(optTeamRole);
+    }
+
+    @PutMapping("/role")
+    @WrapUpResponseBody
+    @ApiOperation("更新角色定义")
+    public void updateOptTeamRole(@RequestBody OptTeamRole optTeamRole) {
+        wfOptService.updateOptTeamRole(optTeamRole);
+    }
+
+    @DeleteMapping("/role/{roleId}")
+    @WrapUpResponseBody
+    @ApiOperation("删除角色定义")
+    public void deleteOptTeamRole(@PathVariable String roleId) {
+        wfOptService.deleteOptTeamRoleById(roleId);
+    }
+
+    @GetMapping("/variables/{optId}")
+    @WrapUpResponseBody
+    @ApiOperation("根据optId获取变量定义列表")
+    public PageQueryResult<OptVariableDefine> listOptVariableDefinesByOptId(@PathVariable String optId, PageDesc pageDesc) {
+        Map<String, Object> filter = CollectionsOpt.createHashMap("optId", optId);
+        List<OptVariableDefine> optVariableDefines = wfOptService.listOptVariableDefinesByFilter(filter, pageDesc);
+        return PageQueryResult.createResult(optVariableDefines, pageDesc);
+    }
+
+    @GetMapping("/variable/{variableId}")
+    @WrapUpResponseBody
+    @ApiOperation("根据主键id获取变量定义")
+    public OptVariableDefine getOptVariableDefineById(@PathVariable String variableId) {
+        return wfOptService.getOptVariableDefineById(variableId);
+    }
+
+    @PostMapping("/variable")
+    @WrapUpResponseBody
+    @ApiOperation("保存变量定义")
+    public void saveOptVariableDefine(@RequestBody OptVariableDefine optVariableDefine) {
+        wfOptService.saveOptVariableDefine(optVariableDefine);
+    }
+
+    @PutMapping("/variable")
+    @WrapUpResponseBody
+    @ApiOperation("更新变量定义")
+    public void updateOptVariableDefine(@RequestBody OptVariableDefine optVariableDefine) {
+        wfOptService.updateOptVariableDefine(optVariableDefine);
+    }
+
+    @DeleteMapping("/variable/{variableId}")
+    @WrapUpResponseBody
+    @ApiOperation("删除变量定义")
+    public void deleteOptVariableDefine(@PathVariable String variableId) {
+        wfOptService.deleteOptVariableDefineById(variableId);
     }
 }
--- centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowOptServiceImpl.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowOptServiceImpl.java
@@ -4,8 +4,12 @@ import com.alibaba.fastjson.JSONArray;
 import com.centit.support.database.utils.PageDesc;
 import com.centit.workflow.dao.FlowOptInfoDao;
 import com.centit.workflow.dao.FlowOptPageDao;
+import com.centit.workflow.dao.OptTeamRoleDao;
+import com.centit.workflow.dao.OptVariableDefineDao;
 import com.centit.workflow.po.FlowOptInfo;
 import com.centit.workflow.po.FlowOptPage;
+import com.centit.workflow.po.OptTeamRole;
+import com.centit.workflow.po.OptVariableDefine;
 import com.centit.workflow.service.FlowOptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +30,12 @@ public class FlowOptServiceImpl implements FlowOptService {
 
     @Autowired
     private FlowOptPageDao flowOptPageDao;
+
+    @Autowired
+    private OptTeamRoleDao optTeamRoleDao;
+
+    @Autowired
+    private OptVariableDefineDao optVariableDefineDao;
 
     @Override
     public List<FlowOptInfo> getListOptInfo() {
@@ -127,6 +137,62 @@ public class FlowOptServiceImpl implements FlowOptService {
     @Override
     public String getOptDefSequenceId() {
         return flowOptPageDao.getOptDefSequenceId();
+    }
+
+    @Override
+    public List<OptTeamRole> listOptTeamRolesByFilter(Map<String, Object> filter, PageDesc pageDesc) {
+        return optTeamRoleDao.listObjects(filter, pageDesc);
+    }
+
+    @Override
+    public OptTeamRole getOptTeamRoleById(String roleId) {
+        return optTeamRoleDao.getObjectById(roleId);
+    }
+
+    @Override
+    @Transactional
+    public void saveOptTeamRole(OptTeamRole optTeamRole) {
+        optTeamRoleDao.saveNewObject(optTeamRole);
+    }
+
+    @Override
+    @Transactional
+    public void updateOptTeamRole(OptTeamRole optTeamRole) {
+        optTeamRoleDao.updateObject(optTeamRole);
+    }
+
+    @Override
+    @Transactional
+    public void deleteOptTeamRoleById(String roleId) {
+        optTeamRoleDao.deleteObjectById(roleId);
+    }
+
+    @Override
+    public List<OptVariableDefine> listOptVariableDefinesByFilter(Map<String, Object> filter, PageDesc pageDesc) {
+        return optVariableDefineDao.listObjects(filter, pageDesc);
+    }
+
+    @Override
+    public OptVariableDefine getOptVariableDefineById(String variableId) {
+        return optVariableDefineDao.getObjectById(variableId);
+    }
+
+    @Override
+    @Transactional
+    public void saveOptVariableDefine(OptVariableDefine optVariableDefine) {
+        optVariableDefineDao.saveNewObject(optVariableDefine);
+    }
+
+    @Override
+    @Transactional
+    public void updateOptVariableDefine(OptVariableDefine optVariableDefine) {
+        optVariableDefineDao.updateObject(optVariableDefine);
+    }
+
+    @Override
+    @Transactional
+    public void deleteOptVariableDefineById(String variableId) {
+        optVariableDefineDao.deleteObjectById(variableId);
     }
 
 }
