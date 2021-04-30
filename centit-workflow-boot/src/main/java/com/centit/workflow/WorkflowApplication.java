218bed7b05cc34dc2cb3f152cf2081065ca180ae
liu_cc <liu_cc@centit.com>
2021-04-30 14:51:14 +0800

    更新

--- centit-workflow-boot/src/main/java/com/centit/workflow/WorkflowApplication.java
+++ centit-workflow-boot/src/main/java/com/centit/workflow/WorkflowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages="com.centit")
+@ComponentScan(basePackages="com.centit")
 public class WorkflowApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
