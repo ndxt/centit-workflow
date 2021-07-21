f1ee0933b3a1d9141c4fb8b5dcdbdd5c7ae71689
codefan <codefan@sina.com>
2021-07-21 10:40:13 +0800

    修改重复提交切面代码，使得其更加通用

--- centit-workflow-module/src/main/java/com/centit/workflow/aop/ResubmitDataAspect.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/aop/ResubmitDataAspect.java
@@ -1,8 +1,7 @@
 package com.centit.workflow.aop;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.centit.workflow.commons.WorkflowException;
+import com.centit.support.algorithm.StringBaseOpt;
+import com.centit.support.common.ObjectException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpSession;
 import java.lang.reflect.Method;
 
 /**
@@ -31,42 +33,40 @@ public class ResubmitDataAspect {
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         //获取注解信息
         NoRepeatCommit annotation = method.getAnnotation(NoRepeatCommit.class);
-        int delaySeconds = annotation.delaySeconds();
-        Object[] pointArgs = joinPoint.getArgs();
-        String key = "";
-        //获取第一个参数
-        Object firstParam = pointArgs[0];
-        //解析参数
-        JSONObject data = (JSONObject) JSON.toJSON(firstParam);
 
-        if (data != null) {
-            StringBuffer sb = new StringBuffer();
-            data.forEach((k, v) -> {
-                if (v != null) {
-                    sb.append(v);
-                }
-            });
-            //生成加密参数 使用了content_MD5的加密方式
-            key = ResubmitLock.handleKey(sb.toString());
+        StringBuilder sb = new StringBuilder(method.getClass()
+            .getName()).append(".").append(method.getName());
+
+        Object[] pointArgs = joinPoint.getArgs();
+        for(Object arg: pointArgs){
+            sb.append(",");
+            if( arg instanceof ServletRequest ||
+               arg instanceof ServletResponse ||
+             arg instanceof HttpSession){
+                sb.append(arg.getClass().getName());
+            } else {
+                sb.append(StringBaseOpt.objectToString(arg));
+            }
         }
+        String key = ResubmitLock.handleKey(sb.toString());
         //执行锁
         boolean lock = false;
         try {
             //设置解锁key
-            logger.info("spring AOP key: {}", key);
+            //logger.info("spring AOP key: {}", key);
             lock = ResubmitLock.getInstance().lock(key, PRESENT);
             if (lock) {
-                logger.info("spring AOP 提交校验submit -> 放行");
+                //logger.info("spring AOP 提交校验submit -> 放行");
                 //放行
                 return joinPoint.proceed();
             } else {
                 logger.info("spring AOP 提交校验submit -> 重复提交");
                 //响应重复提交异常
-                throw new WorkflowException("spring AOP 提交校验submit：重复提交");
+                throw new ObjectException("spring AOP 提交校验submit：重复提交:" + sb );
             }
         } finally {
             //设置解锁key和解锁时间
-            ResubmitLock.getInstance().unLock(lock, key, delaySeconds);
+            ResubmitLock.getInstance().unLock(lock, key, annotation.delaySeconds());
             logger.info("spring AOP 提交校验submit -> 解锁");
         }
     }
