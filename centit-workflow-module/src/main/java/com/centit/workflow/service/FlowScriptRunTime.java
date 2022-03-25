6e2356806fdb65593292aa37ea6a0f7b28d03c1e
codefan <codefan@sina.com>
2022-03-25 14:45:03 +0800

    添加接口 FlowScriptRunTime 重构流程脚本语言

--- centit-workflow-module/src/main/java/com/centit/workflow/service/FlowScriptRunTime.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/FlowScriptRunTime.java
@@ -9,16 +9,16 @@ import java.util.Map;
 public interface FlowScriptRunTime {
     /**
      * 运行流程脚本，流程脚本包括以下几个函数
-     * setValue(name, formula); // 计算变量
-     * saveValue(name, formula); // 计算变量，并保存到流程的变量表中
-     * setNextOptUser(user); // 设置下一个交互节点的操作人员
-     * closeNodes(nodeCode); //根据环节代码关闭节点
+     * setValue(字符串常量 name, 表达式 formula); // 计算变量
+     * saveValue(字符串常量 name, 表达式 formula); // 计算变量，并保存到流程的变量表中
+     * setNextOptUser(表达式 user); // 设置下一个交互节点的操作人员
+     * closeNodes(字符串常量 nodeCode); //根据环节代码关闭节点
      * closeAllIsolatedNodes();// 关闭所有游离节点
      * closeAllOtherNodes(); // 关闭所有其他节点，非本节点全部关闭
-     * deleteFlowWorkTeam(roleCode);//清除角色
-     * assignFlowWorkTeam(roleCode, users);// 设置办件角色
-     * deleteFlowOrganize(roleCode);// 清除机构组
-     * assignFlowOrganize(roleCode, units);// 设置办件机构
+     * deleteFlowWorkTeam(字符串常量 roleCode);//清除角色
+     * assignFlowWorkTeam(字符串常量 roleCode, 表达式 users);// 设置办件角色
+     * deleteFlowOrganize(字符串常量 roleCode);// 清除机构组
+     * assignFlowOrganize(字符串常量 roleCode, 表达式 units);// 设置办件机构
      *
      * @param script  执行的脚本
      * @param flowInst 流程实例
