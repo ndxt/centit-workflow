7664ec06f23fb037d7415d31c8b29e34746d6d6f
codefan <codefan@sina.com>
2022-07-21 13:59:51 +0800

    添加复制多实例节点的接口，代替以前给多人操作节点添加操作人员的操作

--- centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowEngineImpl.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowEngineImpl.java
@@ -563,8 +563,8 @@ public class FlowEngineImpl implements FlowEngine, Serializable {
             //根据权限表达式创建任务列表
             optUsers = UserUnitCalcEngine.calcOperators(context, roleFormula);
             if (optUsers == null || optUsers.size() == 0) {
-                logger.error("权限引擎没有识别出符合表达式的操作人员：" + roleFormula + "，flow_inst_id:" + flowInst.getFlowInstId()
-                    + ",node_inst_id:" + nextOptNode.getNodeId() + "。");
+                logger.debug("权限引擎没有识别出符合表达式的操作人员：" + roleFormula + "，flow_inst_id:" + flowInst.getFlowInstId()
+                    + ",node_inst_id:" + nextOptNode.getNodeId() + "；自动运行节点忽略这个信息。");
             }
         } else if (SysUserFilterEngine.ROLE_TYPE_ITEM.equalsIgnoreCase(nextOptNode.getRoleType())) {
             optUsers = new HashSet<>();
