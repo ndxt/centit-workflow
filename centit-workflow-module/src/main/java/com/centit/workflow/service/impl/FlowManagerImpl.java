031d4be44816abb788fb372a7d7fda0a52588413
zhf <zhf@centit.com>
2025-12-24 10:01:30 +0800

    增大日志返回条数

--- centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowManagerImpl.java
+++ centit-workflow-module/src/main/java/com/centit/workflow/service/impl/FlowManagerImpl.java
@@ -1327,7 +1327,7 @@ public class FlowManagerImpl implements FlowManager, Serializable {
         /*if (!withNodeAction) {
             filterMap.put("optMethod", "flowOpt");
         }*/
-        return optLogManager.listOptLog("workflow", filterMap, -1, -1);
+        return optLogManager.listOptLog("workflow", filterMap, 0, 1000);
     }
 
     /**
